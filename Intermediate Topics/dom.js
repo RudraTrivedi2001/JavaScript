6abc00b2ee842b2b48211bc42e263c988e1761b8
RudraTrivedi2001 <aniruddhatrivediii@gmail.com>
2024-12-02 09:42:07 +0530

    adding new folder

---
+++ Intermediate Topics/dom.js
@@ -0,0 +1,14 @@
+alert("hello") // js is executed first rather than html page
+
+//Q how many ways can js is added in html
+//Q what is window object 
+
+console.log("hello")
+window.console.log("hello2");
+
+//Q what is DOM and why it is used
+console.dir(document)   //prints properties and methods of document obj
+
+console.log(window.document) //prints html code inside document
+
+console.log(document.body)
